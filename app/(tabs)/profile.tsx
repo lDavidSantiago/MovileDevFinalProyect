81ef2c910c0d800a101b5d79b0150c356c5e74df
lDavidSantiago <David.triana@correounivalle.edu.co>
2025-04-02 10:41:09 -0500

    Profile Little Changes

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,46 +1,342 @@
-import { View, Text, TouchableOpacity,TextInput, Image } from 'react-native'
-import React from 'react'
-import { styles } from "../../styles/tabs.styles";
-import { useAuth, useUser } from "@clerk/clerk-expo";
-import { useState } from "react";
-import ThemeButton from '@/components/themeButton';
-import { useTheme } from '@/providers/themeContext';
+import React, { useState } from 'react';
+import * as Linking from 'expo-linking'
 
-import ThemeToggleButton from '@/components/themeButton';
-
-
-
-export default function Profile() {
-  const {signOut} = useAuth();  
-  const {user} = useUser();
-  const [name, setName] = useState("");
-  const userName = user?.fullName || "User";
-  const userProfileImage = user?.imageUrl || require("../../assets/images/profile.png");
-  const {darkMode} = useTheme();
-  return (
-    <View 
-      style={[
-        styles.container,
-        { backgroundColor: darkMode ? "#2E2E2E" : "#fff" },
-      ]}
-      >
-
-      <TouchableOpacity onPress={() => console.log("📷 Change Profile Image")}>
-        <Image source={{uri:userProfileImage}} style={styles.profileImage} /> 
-        </TouchableOpacity>
-        
-        <Text style={styles.userName}>{userName}</Text>
-
-      <TouchableOpacity onPress={() => signOut()}>
-        <Text style={styles.buttonText}>
-        SignOut 
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  TouchableOpacity,
+  Switch,
+  Image,
+} from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import { useClerk, useUser } from '@clerk/clerk-expo';
+import { Link } from 'expo-router';
+export default function Example() {
+  const [form, setForm] = useState({
+    emailNotifications: true,
+    pushNotifications: false,
+    darkTheme: false,
+  });
+  const {signOut} = useClerk();
+  const handleSignOut = async () => {
+    try {
+      await signOut()
+      Linking.openURL(Linking.createURL('/'))
+    } catch (err) {
+      console.error(JSON.stringify(err, null, 2))
+    }
+  }
+  const { user } = useUser();
+    return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#f8f8f8' }}>
+      <View style={styles.header}>
+        <View style={styles.headerAction}>
+          <TouchableOpacity
+            onPress={() => {
+              // handle onPress
+            }}>
+            <FeatherIcon
+              color="#000"
+              name="arrow-left"
+              size={24} />
+          </TouchableOpacity>
+        </View>
+        <Text numberOfLines={1} style={styles.headerTitle}>
+          Profile
         </Text>
-      </TouchableOpacity> 
-      {/*Boton de cambiar de color */}
-      <ThemeButton/>
-  
-    <View>
-    </View>
-    </View> 
-  )
+        <View style={[styles.headerAction, { alignItems: 'flex-end' }]}>
+          <TouchableOpacity
+            onPress={() => {
+              // handle onPress
+            }}>
+            <FeatherIcon
+              color="#000"
+              name="more-vertical"
+              size={24} />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <ScrollView contentContainerStyle={styles.content}>
+        <View style={[styles.section, { paddingTop: 4 }]}>
+          <Text style={styles.sectionTitle}>Account</Text>
+          <View style={styles.sectionBody}>
+            <TouchableOpacity
+              onPress={() => {
+                // handle onPress
+              }}
+              style={styles.profile}>
+              <Image
+                alt=""
+                source={{
+                  uri: user?.imageUrl || 'https://placeholder.com/default-avatar.png'
+                }}
+                style={styles.profileAvatar} />
+              <View style={styles.profileBody}>
+                <Text style={styles.profileName}>{user?.firstName}</Text>
+                <Text style={styles.profileHandle}>{user?.primaryEmailAddress?.emailAddress}</Text>              </View>
+              <FeatherIcon
+                color="#bcbcbc"
+                name="chevron-right"
+                size={22} />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Preferences</Text>
+          <View style={styles.sectionBody}>
+            <View style={[styles.rowWrapper, styles.rowFirst]}>
+              <TouchableOpacity
+                onPress={() => {
+                  // handle onPress
+                }}
+                style={styles.row}>
+                <Text style={styles.rowLabel}>Language</Text>
+                <View style={styles.rowSpacer} />
+                <Text style={styles.rowValue}>English</Text>
+                <FeatherIcon
+                  color="#bcbcbc"
+                  name="chevron-right"
+                  size={19} />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.rowWrapper}>
+              <TouchableOpacity
+                onPress={() => {
+                  // handle onPress
+                }}
+                style={styles.row}>
+                <Text style={styles.rowLabel}>Phone Number</Text>
+                <View style={styles.rowSpacer} />
+                {/*a ? a : b */}
+                <Text style={styles.rowValue}>{(user?.primaryPhoneNumber?.phoneNumber)? user?.primaryPhoneNumber?.phoneNumber : "None"}</Text>
+                <FeatherIcon
+                  color="#bcbcbc"
+                  name="chevron-right"
+                  size={19} />
+              </TouchableOpacity>
+            </View>
+            <View style={styles.rowWrapper}>
+              <View style={styles.row}>
+                <Text style={styles.rowLabel}>Notifications</Text>
+                <View style={styles.rowSpacer} />
+                <Switch
+                  onValueChange={emailNotifications =>
+                    setForm({ ...form, emailNotifications })
+                  }
+                  style={{ transform: [{ scaleX: 0.95 }, { scaleY: 0.95 }] }}
+                  value={form.emailNotifications} />
+              </View>
+            </View>
+            <View style={styles.rowWrapper}>
+              <View style={styles.row}>
+                <Text style={styles.rowLabel}>Dark Theme</Text>
+                <View style={styles.rowSpacer} />
+                <Switch
+                  onValueChange={darkTheme =>
+                    setForm({ ...form, darkTheme })
+                  }
+                  style={{ transform: [{ scaleX: 0.95 }, { scaleY: 0.95 }] }}
+                  value={form.darkTheme} />
+              </View>
+            </View>
+            
+          </View>
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.sectionTitle}>Resources</Text>
+          <View style={styles.sectionBody}>
+            <View style={[styles.rowWrapper, styles.rowFirst]}>
+              <TouchableOpacity
+                onPress={() => {
+                  Linking.openURL('https://github.com/lDavidSantiago?tab=repositories')
+                }}
+                style={styles.row}>
+                <Text style={styles.rowLabel}>Contact Us</Text>
+                <View style={styles.rowSpacer} />
+                <FeatherIcon
+                  color="#bcbcbc"
+                  name="chevron-right"
+                  size={19} />
+              </TouchableOpacity>
+            </View>
+            
+            
+            <View style={[styles.rowWrapper, styles.rowLast]}>
+              <TouchableOpacity
+                onPress={() => {
+                  // handle onPress
+                }}
+                style={styles.row}>
+                <Text style={styles.rowLabel}>Terms and Privacy</Text>
+                <View style={styles.rowSpacer} />
+                <FeatherIcon
+                  color="#bcbcbc"
+                  name="chevron-right"
+                  size={19} />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <View style={styles.section}>
+          <View style={styles.sectionBody}>
+            <View
+              style={[
+                styles.rowWrapper,
+                styles.rowFirst,
+                styles.rowLast,
+                { alignItems: 'center' },
+              ]}>
+              <TouchableOpacity
+                onPress={() => {
+                  handleSignOut()
+                }}
+                style={styles.row}>
+                <Text style={[styles.rowLabel, styles.rowLabelLogout]}>
+                  Log Out
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+        <Text style={styles.contentFooter}>App Version 2.24 #50491</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
+const styles = StyleSheet.create({
+  /** Header */
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingHorizontal: 16,
+  },
+  headerAction: {
+    width: 40,
+    height: 40,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+  },
+  headerTitle: {
+    fontSize: 19,
+    fontWeight: '600',
+    color: '#000',
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+    textAlign: 'center',
+  },
+  /** Content */
+  content: {
+    paddingHorizontal: 16,
+  },
+  contentFooter: {
+    marginTop: 24,
+    fontSize: 13,
+    fontWeight: '500',
+    textAlign: 'center',
+    color: '#a69f9f',
+  },
+  /** Section */
+  section: {
+    paddingVertical: 12,
+  },
+  sectionTitle: {
+    margin: 8,
+    marginLeft: 12,
+    fontSize: 13,
+    letterSpacing: 0.33,
+    fontWeight: '500',
+    color: '#a69f9f',
+    textTransform: 'uppercase',
+  },
+  sectionBody: {
+    borderRadius: 12,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+  },
+  /** Profile */
+  profile: {
+    padding: 12,
+    backgroundColor: '#fff',
+    borderRadius: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  profileAvatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 9999,
+    marginRight: 12,
+  },
+  profileBody: {
+    marginRight: 'auto',
+  },
+  profileName: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#292929',
+  },
+  profileHandle: {
+    marginTop: 2,
+    fontSize: 16,
+    fontWeight: '400',
+    color: '#858585',
+  },
+  /** Row */
+  row: {
+    height: 44,
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    paddingRight: 12,
+  },
+  rowWrapper: {
+    paddingLeft: 16,
+    backgroundColor: '#fff',
+    borderTopWidth: 1,
+    borderColor: '#f0f0f0',
+  },
+  rowFirst: {
+    borderTopLeftRadius: 12,
+    borderTopRightRadius: 12,
+  },
+  rowLabel: {
+    fontSize: 16,
+    letterSpacing: 0.24,
+    color: '#000',
+  },
+  rowSpacer: {
+    flexGrow: 1,
+    flexShrink: 1,
+    flexBasis: 0,
+  },
+  rowValue: {
+    fontSize: 16,
+    fontWeight: '500',
+    color: '#ababab',
+    marginRight: 4,
+  },
+  rowLast: {
+    borderBottomLeftRadius: 12,
+    borderBottomRightRadius: 12,
+  },
+  rowLabelLogout: {
+    width: '100%',
+    textAlign: 'center',
+    fontWeight: '600',
+    color: '#dc2626',
+  },
+});
